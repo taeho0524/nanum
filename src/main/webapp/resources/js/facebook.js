089c3ac03b2be34c797d0d6eac121c5f3fe9cbde
soomti <soom93@gmail.com>
2016-06-10 15:22:57 +0900

    why dont do that pull?

--- src/main/webapp/resources/js/facebook.js
+++ src/main/webapp/resources/js/facebook.js
@@ -11,22 +11,15 @@
 		      xfbml      : true,		
 		      version    : 'v2.5'
 		    });
-		    FB.getLoginStatus(function(response) {
-		    	if (response.status === 'connected') {
-		    		document.getElementById('status').innerHTML = 'We are connected.';
-		    		document.getElementById('login').style.visibility = 'hidden';
-		    	} else if (response.status === 'not_authorized') {
-		    		document.getElementById('status').innerHTML = 'We are not logged in.'
-		    	} else {
-		    		document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
-		    	}
-		    });
+		    
 		};
+		
+		
 		(function(d, s, id){
 		    var js, fjs = d.getElementsByTagName(s)[0];
 		    if (d.getElementById(id)) {return;}
 		    js = d.createElement(s); js.id = id;
-		    js.src = "//connect.facebook.net/ko_KR/sdk.js";
+		    js.src = "//connect.facebook.net/ko_KR/sdk.js"; //언어 설정
 		    fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
 		
@@ -51,8 +44,9 @@
 		// getting basic user info
 		function getInfo() {
 			alert('getinfo');
-			FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture,email'},function(response) {
+			FB.api('/me', 'GET', {fields: 'email'},function(response) {
 				document.getElementById('status').innerHTML = response.name;
-				alert(response.name);
+				alert(response.email);
+				location.href="signup.nn";  
 			});
 		}
